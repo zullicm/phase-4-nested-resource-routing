9d56be4f36dbe8543873d4bd6e9f6610d5ba2031
Christopher Marzulli <chriszulli15@gmail.com>
2022-10-13 13:18:59 -0400

    commit tree bc21f24dd1e1636e9f20b0ab0a95035051aafdab

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :dog_houses, only: [:show]
   resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/dog_houses/:dog_house_id/reviews', to: 'dog_houses#reviews_index'
+  get '/dog_houses/:dog_house_id/reviews/:id', to: 'dog_houses#review'
 end
